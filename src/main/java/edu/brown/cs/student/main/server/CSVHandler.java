19532de3070014f2125c8f77976a8d7885e27d6b
Matthew Yoon <ymatthew2466@gmail.comgit config --global user.name Matthew Yoon>
2024-02-12 22:32:53 -0500

    user story 2 API

--- src/main/java/edu/brown/cs/student/main/server/CSVHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/CSVHandler.java
@@ -16,11 +16,19 @@ import spark.Route;
 
 public class CSVHandler implements Route {
 
+  private HashMap<String, String> StateMap;
+
   @Override
   public Object handle(Request request, Response response) {
-    Set<String> params = request.queryParams();
-    String participants = request.queryParams("");
 
+    InitializeStateMap();
+
+    Set<String> params = request.queryParams();
+    String StateID = request.queryParams("state");
+    StateID = StateID.replaceAll(" ", "").toLowerCase();
+    System.out.println(StateID);
+
+    // variables ? City = SanFran
     for (int i = 0; i < params.size(); i++) {
       System.out.println("Parameter (" + i + "): " + params.toArray()[i].toString());
     }
@@ -29,15 +37,19 @@ public class CSVHandler implements Route {
     Map<String, Object> responseMap = new HashMap<>();
     try {
       // Sends a request to the API and receives JSON back
-      String countyJson = this.sendRequest(participants);
+      String countyJson = this.sendRequest(StateID);
       //      System.out.println("Result: " + activityJson);
       // Deserializes JSON into an Activity
       //          Activity activity = ActivityAPIUtilities.deserializeActivity(activityJson);
-      String[] counties = CountyDataUtilities.deserializeCounty(countyJson);
+      String[][] CountyData = CountyDataUtilities.deserializeCounty(countyJson);
 
-      for (int i = 0; i < counties.length; i++) {
-        System.out.println("County (" + i + "): " + counties[i]);
+      for (int i = 0; i < CountyData.length; i++) {
+        for (int j = 0; j < CountyData[i].length; j++) {
+          System.out.print(CountyData[i][j] + "\t");
+        }
+        System.out.println();
       }
+
       // Adds results to the responseMap
       responseMap.put("result", "success");
       //    responseMap.put("activity", activity);
@@ -50,21 +62,27 @@ public class CSVHandler implements Route {
       // falls short.
       responseMap.put("result", "Exception");
     }
+
     return responseMap;
   }
 
-  private String sendRequest(String participants)
+  private String sendRequest(String stateID)
       throws URISyntaxException, IOException, InterruptedException {
     // Build a request to this BoredAPI. Try out this link in your browser, what do you see?
     // TODO 1: Looking at the documentation, how can we add to the URI to query based
     // on participant number?
+
     HttpRequest buildBoredApiRequest =
         HttpRequest.newBuilder()
             .uri(
                 new URI(
-                    "https://api.census.gov/data/2021/acs/acs1/subject/variables?get=NAME,S2802_C03_022E&for=county:*&in=state:06"))
+                    "https://api.census.gov/data/2021/acs/acs1/subject/variables?get=NAME,S2802_C03_022E&for=county:*&in=state:"
+                        + this.StateMap.get(stateID.trim().toLowerCase())))
             .GET()
             .build();
+    // link.gov/...? + variable +"/" + var2
+    // localhost/loadcsv?NAME=xxx,COUNTY=xxx
+    // convert xxx -> link
 
     // Send that API request then store the response in this variable. Note the generic type.
     HttpResponse<String> sentBoredApiResponse =
@@ -78,5 +96,72 @@ public class CSVHandler implements Route {
     //    System.out.println(sentBoredApiResponse.body());
 
     return sentBoredApiResponse.body();
+  }
+
+  private void InitializeStateMap() {
+
+    this.StateMap = new HashMap<>();
+
+    String[][] data = {
+      {"Alabama", "01"},
+      {"Alaska", "02"},
+      {"Arizona", "04"},
+      {"Arkansas", "05"},
+      {"California", "06"},
+      {"Louisiana", "22"},
+      {"Kentucky", "21"},
+      {"Colorado", "08"},
+      {"Connecticut", "09"},
+      {"Delaware", "10"},
+      {"District of Columbia", "11"},
+      {"Florida", "12"},
+      {"Georgia", "13"},
+      {"Hawaii", "15"},
+      {"Idaho", "16"},
+      {"Illinois", "17"},
+      {"Indiana", "18"},
+      {"Iowa", "19"},
+      {"Kansas", "20"},
+      {"Maine", "23"},
+      {"Maryland", "24"},
+      {"Massachusetts", "25"},
+      {"Michigan", "26"},
+      {"Minnesota", "27"},
+      {"Mississippi", "28"},
+      {"Missouri", "29"},
+      {"Montana", "30"},
+      {"Nebraska", "31"},
+      {"Nevada", "32"},
+      {"New Hampshire", "33"},
+      {"New Jersey", "34"},
+      {"New Mexico", "35"},
+      {"New York", "36"},
+      {"North Carolina", "37"},
+      {"North Dakota", "38"},
+      {"Ohio", "39"},
+      {"Oklahoma", "40"},
+      {"Oregon", "41"},
+      {"Pennsylvania", "42"},
+      {"Rhode Island", "44"},
+      {"South Carolina", "45"},
+      {"South Dakota", "46"},
+      {"Tennessee", "47"},
+      {"Texas", "48"},
+      {"Utah", "49"},
+      {"Vermont", "50"},
+      {"Virginia", "51"},
+      {"Washington", "53"},
+      {"West Virginia", "54"},
+      {"Wisconsin", "55"},
+      {"Wyoming", "56"},
+      {"Puerto Rico", "72"}
+    };
+
+    for (String[] entry : data) {
+      if (entry.length >= 2) {
+        entry[0] = entry[0].replaceAll(" ", "").toLowerCase();
+        this.StateMap.put(entry[0], entry[1]);
+      }
+    }
   }
 }
--- src/main/java/edu/brown/cs/student/main/utils/CountyDataUtilities.java
+++ src/main/java/edu/brown/cs/student/main/utils/CountyDataUtilities.java
@@ -2,25 +2,59 @@ package edu.brown.cs.student.main.utils;
 
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
-import com.squareup.moshi.Types;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.List;
 
 public class CountyDataUtilities {
-  public static String[] deserializeCounty(String jsonCounty) {
+
+  //  public static String deserializeCounty(String jsonCounty) {
+  //
+  //    Moshi moshi = new Moshi.Builder().build();
+  //    Type type = Types.newParameterizedType(List.class, Types.newParameterizedType(List.class,
+  // String.class));
+  //    JsonAdapter<List<List<String>>> adapter = moshi.adapter(type);
+  //
+  //    try {
+  //      List<List<String>> rawData = adapter.fromJson(jsonCounty);
+  //      return rawData;
+  //
+  //    } catch (IOException e) {
+  //      e.printStackTrace();
+  //      return "";
+  //    }
+  //  }
+  public static String[][] deserializeCounty(String jsonCounty) {
     try {
       Moshi moshi = new Moshi.Builder().build();
-
-      Type type = Types.newParameterizedType(List.class, String.class);
-      JsonAdapter<List<String>> adapter = moshi.adapter(type);
-      System.out.println("r: " + adapter.fromJson(jsonCounty));
-
-      return new String[0];
-
+      JsonAdapter<String[][]> adapter = moshi.adapter(String[][].class);
+      String[][] data = adapter.fromJson(jsonCounty);
+      return data;
     } catch (IOException e) {
       e.printStackTrace();
-      return new String[0];
+      return null;
     }
   }
 }
+
+/*
+try {
+
+  Moshi moshi = new Moshi.Builder().build();
+  JsonAdapter<String> adapter = moshi.adapter(String.class);
+  String row = adapter.fromJson(jsonCounty);
+  return row;
+
+} catch (IOException e) {
+  e.printStackTrace();
+  return "";
+}*/
+
+/**
+ * Moshi moshi = new Moshi.Builder().build();
+ *
+ * <p>// Initializes an adapter to an Activity class then uses it to parse the JSON.
+ * JsonAdapter<String> adapter = moshi.adapter(String.class);
+ *
+ * <p>String row = adapter.fromJson(jsonActivity);
+ *
+ * <p>return activity;
+ */
